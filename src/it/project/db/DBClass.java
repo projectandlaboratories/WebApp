44bf9546e523b645384e2032192f25f2dc021167
lako95 <s249804@studenti.polito.it>
2019-06-08 17:36:09 +0200

    implementazione getProfiles

--- src/it/project/db/DBClass.java
+++ src/it/project/db/DBClass.java
@@ -5,11 +5,16 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import it.project.dto.Interval;
 import it.project.dto.Profile;
+import it.project.enums.DayMoment;
+import it.project.enums.DayName;
+import it.project.enums.DayType;
 import it.project.utils.DbIdentifiers;
 
 public class DBClass {
@@ -73,24 +78,36 @@ public class DBClass {
 	
 	}
 	
-//	public static List<Profile> getProfileList(){
-//		
-//		List<Profile> profiles = new ArrayList<>();
-//		Statement statement;
-//		try {
-//			statement = conn.createStatement();
-//			String query = "SELECT * from profiles";
-//			ResultSet result = statement.executeQuery(query);
-//			if (result.next()) {
-//				
-//			}
-//		} catch (SQLException e) {
-//			
-//			e.printStackTrace();
-//		}
-//		
-//		return valore;
-//		return null;
-//	}
+	public static List<Profile> getProfileList(){
+		
+		List<Profile> profiles = new ArrayList<>();
+		Statement statement;
+		try {
+			statement = conn.createStatement();
+			String query = "SELECT * from profiles";
+			ResultSet result = statement.executeQuery(query);
+			if (result.next()) {
+				String idProfile = result.getString("ID_PROFILE");
+				DayName dayOfWeek = DayName.valueOf(result.getString("DAY_OF_WEEK"));
+				DayType dayType = DayType.valueOf(result.getString("DAY_TYPE"));
+				DayMoment dayMoment = DayMoment.valueOf(result.getString("DAY_MOMENT"));
+				int startHour = result.getInt("START_HOUR");
+				int startMin = result.getInt("START_MIN");
+				int endHour = result.getInt("END_HOUR");
+				int endMin = result.getInt("END_MIN");
+				double temperature = result.getDouble("TEMPERATURE");
+				
+				Interval interval = new Interval(startHour,startMin,endHour,endMin,temperature,dayMoment);
+				Profile profile = new Profile(idProfile,dayOfWeek,dayType,interval);
+				profiles.add(profile);				
+			}
+		
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 	
 }
